4546c1745d5b8cf09880571d9c0399385be8c3c7
justasdaugintis <justasd101@gmail.com>
2020-09-24 10:03:44 +0300

    coffee billboard component

---
+++ src/components/coffeeBillboard/coffeeBillboard.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { IRootState } from "../../reducer/combinedReducer";
+import { ICoffee } from "../../sharedInterfaces/ICoffee";
+import CoffeeElement from "./coffeeElement";
+import { coffeeActions } from "./reduxSlice";
+
+/**
+ *  Component responsible rendering all of the coffee items
+ */
+const CoffeeBillboard: React.FC = () => {
+
+  const dispatch = useDispatch();
+
+  /**
+   *  Coffee data from redux
+   */
+  const { coffees } = useSelector((state: IRootState) => state.coffeeData);
+
+  /**
+   *  Function which calls dispatch on the remove coffee item action. 
+   */
+  const removeItem = (id: string)=>{
+    dispatch(coffeeActions.removeItem(id))
+  }
+
+  /**
+   *  Array of coffee item components to be displayed. 
+   */
+  const items = coffees? coffees.map((coffee: ICoffee) =>
+    <CoffeeElement 
+      key={coffee.id}
+      id={coffee.id} 
+      name={coffee.name} 
+      img={coffee.img}
+      price={coffee.price}
+      onRemove={removeItem}
+    />
+  ) : []
+
+  return (
+    <div className="coffee-Billboard">
+      {items}
+    </div>
+  );
+};
+
+export default CoffeeBillboard;
---
+++ src/components/coffeeBillboard/coffeeElement.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Button from "react-bootstrap/esm/Button";
+import Image from "react-bootstrap/esm/Image";
+import "./style.scss"
+
+interface IProps {
+   id: string;
+   name: string;
+   price: string;
+   img?: string;
+   onRemove: Function;
+}
+
+/**
+ * Component responsible for showing an instance of a coffee item on the billboard
+ * @param id contains id of coffee item 
+ * @param name contains name of coffee
+ * @param price contains price of coffee
+ * @param img image url
+ * @param onRemove contains function to remove a coffee item from the billboard
+ */
+const CoffeeElement: React.FC<IProps> = ({id, name, price, img, onRemove}) => {
+  return (
+      <div key={id+1} className="coffeeElement">
+        <div  className="removeButton">
+          <Button size="sm" variant="danger" onClick={()=>{onRemove(id)}}>X</Button>
+        </div>
+          <Image src={img} alt={"...this should be an image of a coffee"} thumbnail />
+          <div className="coffee-details">
+            <p className="coffee-price" >{price}</p>
+            <p className="coffee-name">{name}</p>
+          </div>
+      </div>
+  )
+};
+
+export default CoffeeElement
